ebeb0df2e1d7ff7a27a4bdd9299dc6756131847c
dougwhittingham <60674356+dougwhittingham@users.noreply.github.com>
2020-11-24 23:46:25 -0800

    Add files via upload
    Initial testing script for rfm98 and Raspberry Pi.

---
+++ raspi_rfm_test.py
@@ -0,0 +1,20 @@
+import time
+import digitalio
+import board
+import busio
+import adafruit_rfm9x
+
+RADIO_FREQ_MHZ = 433.0
+CS = digitalio.DigitalInOut(board.D5)
+RESET = digitalio.DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+
+# Initialze RFM radio with a more conservative baudrate
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, baudrate=1000000)
+
+
+for i in range(duration):
+	print('Sending packet of 8 bytes...')
+		rfm9x.send(bytes([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07]))
+		time.sleep(1)
