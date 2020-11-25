544bc113acc3f3a44662b83d2bb020611a76b96c
dougwhittingham <60674356+dougwhittingham@users.noreply.github.com>
2020-11-24 23:40:39 -1000

    Update raspi_rfm_test.py

--- raspi_rfm_test.py
+++ raspi_rfm_test.py
@@ -2,21 +2,18 @@ import time
 import digitalio
 import board
 import busio
+import RPi.GPIO as io
 import adafruit_rfm9x
-
 
 RADIO_FREQ_MHZ = 433.0
 CS = digitalio.DigitalInOut(board.D5)
 RESET = digitalio.DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
-duration = 25
 
 # Initialze RFM radio with a more conservative baudrate
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, baudrate=1000000)
 
 
-for i in range(duration):
-	print('Sending packet of 8 bytes...')
-		rfm9x.send(bytes([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07]))
-		time.sleep(1)
+
+print(rfm9x._read_u8(_RH_RF95_REG_00_FIFO))
